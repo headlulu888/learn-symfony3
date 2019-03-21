7c1d0add13617641ea7746edf24333a8d13d72e8
headlulu888 <headlulu888@gmail.com>
2019-03-21 19:33:21 +0300

    routing wildcards

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ray-pc
+ * Date: 21/03/2019
+ * Time: 19:21
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        return new Response('The genus: ' . $genusName);
+    }
+}
